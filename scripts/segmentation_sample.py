853f5173f7fff33c40eb32a87236cfe182b1d67f
hoangtung386 <levuhoangtung1542003@gmail.com>
2025-12-06 23:09:27 +0700

    Update segmentation_sample.py

--- scripts/segmentation_sample.py
+++ scripts/segmentation_sample.py
@@ -1,11 +1,213 @@
+# import argparse
+# import os
+# from ssl import OP_NO_TLSv1
+# import nibabel as nib
+# # from visdom import Visdom
+# # viz = Visdom(port=8850)
+# import sys
+# import random
+# sys.path.append(".")
+# import numpy as np
+# import time
+# import torch as th
+# from PIL import Image
+# import torch.distributed as dist
+# from guided_diffusion import dist_util, logger
+# from guided_diffusion.bratsloader import BRATSDataset, BRATSDataset3D
+# from guided_diffusion.isicloader import ISICDataset
+# from guided_diffusion.custom_dataset_loader import CustomDataset
+# import torchvision.utils as vutils
+# from guided_diffusion.utils import staple
+# from guided_diffusion.script_util import (
+#     NUM_CLASSES,
+#     model_and_diffusion_defaults,
+#     create_model_and_diffusion,
+#     add_dict_to_argparser,
+#     args_to_dict,
+# )
+# import torchvision.transforms as transforms
+# from torchsummary import summary
+# seed=10
+# th.manual_seed(seed)
+# th.cuda.manual_seed_all(seed)
+# np.random.seed(seed)
+# random.seed(seed)
+
+# def visualize(img):
+#     _min = img.min()
+#     _max = img.max()
+#     normalized_img = (img - _min)/ (_max - _min)
+#     return normalized_img
+
+
+# def main():
+#     args = create_argparser().parse_args()
+#     dist_util.setup_dist(args)
+#     logger.configure(dir = args.out_dir)
+
+#     if args.data_name == 'ISIC':
+#         tran_list = [transforms.Resize((args.image_size,args.image_size)), transforms.ToTensor(),]
+#         transform_test = transforms.Compose(tran_list)
+
+#         ds = ISICDataset(args, args.data_dir, transform_test, mode = 'Test')
+#         args.in_ch = 4
+#     elif args.data_name == 'BRATS':
+#         tran_list = [transforms.Resize((args.image_size,args.image_size)),]
+#         transform_test = transforms.Compose(tran_list)
+
+#         ds = BRATSDataset3D(args.data_dir,transform_test)
+#         args.in_ch = 5
+#     else:
+#         tran_list = [transforms.Resize((args.image_size,args.image_size)), transforms.ToTensor()]
+#         transform_test = transforms.Compose(tran_list)
+
+#         ds = CustomDataset(args, args.data_dir, transform_test, mode = 'Test')
+#         args.in_ch = 4
+
+#     datal = th.utils.data.DataLoader(
+#         ds,
+#         batch_size=args.batch_size,
+#         shuffle=True)
+#     data = iter(datal)
+
+#     logger.log("creating model and diffusion...")
+
+#     model, diffusion = create_model_and_diffusion(
+#         **args_to_dict(args, model_and_diffusion_defaults().keys())
+#     )
+#     all_images = []
+
+
+#     state_dict = dist_util.load_state_dict(args.model_path, map_location="cpu")
+#     from collections import OrderedDict
+#     new_state_dict = OrderedDict()
+#     for k, v in state_dict.items():
+#         # name = k[7:] # remove `module.`
+#         if 'module.' in k:
+#             new_state_dict[k[7:]] = v
+#             # load params
+#         else:
+#             new_state_dict = state_dict
+
+#     model.load_state_dict(new_state_dict)
+
+#     model.to(dist_util.dev())
+#     if args.use_fp16:
+#         model.convert_to_fp16()
+#     model.eval()
+#     for _ in range(len(data)):
+#         batch, m, path = next(data)  #should return an image from the dataloader "data"
+#         if isinstance(batch, (list, tuple)):
+#             b, b_2_5d = batch
+#         else:
+#             b = batch
+#             b_2_5d = None
+#         c = th.randn_like(b[:, :1, ...])
+#         img = th.cat((b, c), dim=1)     #add a noise channel$
+#         if args.data_name == 'ISIC':
+#             slice_ID=path[0].split("_")[-1].split('.')[0]
+#         elif args.data_name == 'BRATS':
+#             # slice_ID=path[0].split("_")[2] + "_" + path[0].split("_")[4]
+#             slice_ID=path[0].split("_")[-3] + "_" + path[0].split("slice")[-1].split('.nii')[0]
+
+#         logger.log("sampling...")
+
+#         start = th.cuda.Event(enable_timing=True)
+#         end = th.cuda.Event(enable_timing=True)
+#         enslist = []
+
+#         for i in range(args.num_ensemble):  #this is for the generation of an ensemble of 5 masks.
+#             model_kwargs = {}
+#             if b_2_5d is not None:
+#                 model_kwargs["x_2_5d"] = b_2_5d.to(dist_util.dev())
+#             start.record()
+#             sample_fn = (
+#                 diffusion.p_sample_loop_known if not args.use_ddim else diffusion.ddim_sample_loop_known
+#             )
+#             sample, x_noisy, org, cal, cal_out = sample_fn(
+#                 model,
+#                 (args.batch_size, 3, args.image_size, args.image_size), img,
+#                 step = args.diffusion_steps,
+#                 clip_denoised=args.clip_denoised,
+#                 model_kwargs=model_kwargs,
+#             )
+
+#             end.record()
+#             th.cuda.synchronize()
+#             print('time for 1 sample', start.elapsed_time(end))  #time measurement for the generation of 1 sample
+
+#             co = th.tensor(cal_out)
+#             if args.version == 'new':
+#                 enslist.append(sample[:,-1,:,:])
+#             else:
+#                 enslist.append(co)
+
+#             if args.debug:
+#                 # print('sample size is',sample.size())
+#                 # print('org size is',org.size())
+#                 # print('cal size is',cal.size())
+#                 if args.data_name == 'ISIC':
+#                     # s = th.tensor(sample)[:,-1,:,:].unsqueeze(1).repeat(1, 3, 1, 1)
+#                     o = th.tensor(org)[:,:-1,:,:]
+#                     c = th.tensor(cal).repeat(1, 3, 1, 1)
+#                     # co = co.repeat(1, 3, 1, 1)
+
+#                     s = sample[:,-1,:,:]
+#                     b,h,w = s.size()
+#                     ss = s.clone()
+#                     ss = ss.view(s.size(0), -1)
+#                     ss -= ss.min(1, keepdim=True)[0]
+#                     ss /= ss.max(1, keepdim=True)[0]
+#                     ss = ss.view(b, h, w)
+#                     ss = ss.unsqueeze(1).repeat(1, 3, 1, 1)
+
+#                     tup = (ss,o,c)
+#                 elif args.data_name == 'BRATS':
+#                     s = th.tensor(sample)[:,-1,:,:].unsqueeze(1)
+#                     m = th.tensor(m.to(device = 'cuda:0'))[:,0,:,:].unsqueeze(1)
+#                     o1 = th.tensor(org)[:,0,:,:].unsqueeze(1)
+#                     o2 = th.tensor(org)[:,1,:,:].unsqueeze(1)
+#                     o3 = th.tensor(org)[:,2,:,:].unsqueeze(1)
+#                     o4 = th.tensor(org)[:,3,:,:].unsqueeze(1)
+#                     c = th.tensor(cal)
+
+#                     tup = (o1/o1.max(),o2/o2.max(),o3/o3.max(),o4/o4.max(),m,s,c,co)
+
+#                 compose = th.cat(tup,0)
+#                 vutils.save_image(compose, fp = os.path.join(args.out_dir, str(slice_ID)+'_output'+str(i)+".jpg"), nrow = 1, padding = 10)
+#         ensres = staple(th.stack(enslist,dim=0)).squeeze(0)
+#         vutils.save_image(ensres, fp = os.path.join(args.out_dir, str(slice_ID)+'_output_ens'+".jpg"), nrow = 1, padding = 10)
+
+# def create_argparser():
+#     defaults = dict(
+#         data_name = 'BRATS',
+#         data_dir="../dataset/brats2020/testing",
+#         clip_denoised=True,
+#         num_samples=1,
+#         batch_size=1,
+#         use_ddim=False,
+#         model_path="",         #path to pretrain model
+#         num_ensemble=5,      #number of samples in the ensemble
+#         gpu_dev = "0",
+#         out_dir='./results/',
+#         multi_gpu = None, #"0,1,2"
+#         debug = False
+#     )
+#     defaults.update(model_and_diffusion_defaults())
+#     parser = argparse.ArgumentParser()
+#     add_dict_to_argparser(parser, defaults)
+#     return parser
+
+
+# if __name__ == "__main__":
+
+#     main()
 
 
 import argparse
 import os
 from ssl import OP_NO_TLSv1
 import nibabel as nib
-# from visdom import Visdom
-# viz = Visdom(port=8850)
 import sys
 import random
 sys.path.append(".")
@@ -17,7 +219,7 @@ import torch.distributed as dist
 from guided_diffusion import dist_util, logger
 from guided_diffusion.bratsloader import BRATSDataset, BRATSDataset3D
 from guided_diffusion.isicloader import ISICDataset
-from guided_diffusion.custom_dataset_loader import CustomDataset
+from guided_diffusion.custom_dataset_loader import CustomDataset, CustomDataset3D
 import torchvision.utils as vutils
 from guided_diffusion.utils import staple
 from guided_diffusion.script_util import (
@@ -29,6 +231,8 @@ from guided_diffusion.script_util import (
 )
 import torchvision.transforms as transforms
 from torchsummary import summary
+from pathlib import Path
+
 seed=10
 th.manual_seed(seed)
 th.cuda.manual_seed_all(seed)
@@ -47,29 +251,45 @@ def main():
     dist_util.setup_dist(args)
     logger.configure(dir = args.out_dir)
 
+    # ===== FIX: Match the logic from segmentation_train.py =====
+    logger.log("creating data loader...")
+    
     if args.data_name == 'ISIC':
         tran_list = [transforms.Resize((args.image_size,args.image_size)), transforms.ToTensor(),]
         transform_test = transforms.Compose(tran_list)
-
         ds = ISICDataset(args, args.data_dir, transform_test, mode = 'Test')
         args.in_ch = 4
+        
     elif args.data_name == 'BRATS':
         tran_list = [transforms.Resize((args.image_size,args.image_size)),]
         transform_test = transforms.Compose(tran_list)
-
-        ds = BRATSDataset3D(args.data_dir,transform_test)
+        ds = BRATSDataset(args.data_dir, transform_test, test_flag=True)
         args.in_ch = 5
+        
+    elif args.data_name == 'BRATS3D':
+        tran_list = [transforms.Resize((args.image_size,args.image_size)),]
+        transform_test = transforms.Compose(tran_list)
+        ds = BRATSDataset3D(args.data_dir, transform_test, test_flag=True)
+        args.in_ch = 5
+        
+    elif any(Path(args.data_dir).glob("*/*.nii.gz")):
+        tran_list = [transforms.Resize((args.image_size,args.image_size)),]
+        transform_test = transforms.Compose(tran_list)
+        print("Your current directory (3D NIfTI detected):", args.data_dir)
+        ds = CustomDataset3D(args, args.data_dir, transform_test, mode='Test')
+        args.in_ch = 4
+        
     else:
         tran_list = [transforms.Resize((args.image_size,args.image_size)), transforms.ToTensor()]
         transform_test = transforms.Compose(tran_list)
-
+        print("Your current directory (2D images):", args.data_dir)
         ds = CustomDataset(args, args.data_dir, transform_test, mode = 'Test')
         args.in_ch = 4
 
     datal = th.utils.data.DataLoader(
         ds,
         batch_size=args.batch_size,
-        shuffle=True)
+        shuffle=False)  # Changed to False for consistent testing
     data = iter(datal)
 
     logger.log("creating model and diffusion...")
@@ -79,81 +299,88 @@ def main():
     )
     all_images = []
 
-
     state_dict = dist_util.load_state_dict(args.model_path, map_location="cpu")
     from collections import OrderedDict
     new_state_dict = OrderedDict()
     for k, v in state_dict.items():
-        # name = k[7:] # remove `module.`
         if 'module.' in k:
             new_state_dict[k[7:]] = v
-            # load params
         else:
             new_state_dict = state_dict
+            break
 
     model.load_state_dict(new_state_dict)
-
     model.to(dist_util.dev())
+    
     if args.use_fp16:
         model.convert_to_fp16()
     model.eval()
-    for _ in range(len(data)):
-        batch, m, path = next(data)  #should return an image from the dataloader "data"
+    
+    logger.log(f"Processing {len(datal)} batches...")
+    
+    for batch_idx in range(len(datal)):
+        try:
+            batch, m, path = next(data)
+        except StopIteration:
+            break
+            
         if isinstance(batch, (list, tuple)):
             b, b_2_5d = batch
         else:
             b = batch
             b_2_5d = None
+            
         c = th.randn_like(b[:, :1, ...])
-        img = th.cat((b, c), dim=1)     #add a noise channel$
+        img = th.cat((b, c), dim=1)
+        
+        # Extract slice ID based on data type
         if args.data_name == 'ISIC':
-            slice_ID=path[0].split("_")[-1].split('.')[0]
-        elif args.data_name == 'BRATS':
-            # slice_ID=path[0].split("_")[2] + "_" + path[0].split("_")[4]
-            slice_ID=path[0].split("_")[-3] + "_" + path[0].split("slice")[-1].split('.nii')[0]
+            slice_ID = path[0].split("_")[-1].split('.')[0]
+        elif args.data_name in ['BRATS', 'BRATS3D']:
+            slice_ID = path[0].split("_")[-3] + "_" + path[0].split("slice")[-1].split('.nii')[0]
+        else:
+            slice_ID = Path(path[0]).stem
 
-        logger.log("sampling...")
+        logger.log(f"Sampling batch {batch_idx+1}/{len(datal)} - {slice_ID}...")
 
         start = th.cuda.Event(enable_timing=True)
         end = th.cuda.Event(enable_timing=True)
         enslist = []
 
-        for i in range(args.num_ensemble):  #this is for the generation of an ensemble of 5 masks.
+        for i in range(args.num_ensemble):
             model_kwargs = {}
             if b_2_5d is not None:
                 model_kwargs["x_2_5d"] = b_2_5d.to(dist_util.dev())
+                
             start.record()
             sample_fn = (
                 diffusion.p_sample_loop_known if not args.use_ddim else diffusion.ddim_sample_loop_known
             )
             sample, x_noisy, org, cal, cal_out = sample_fn(
                 model,
-                (args.batch_size, 3, args.image_size, args.image_size), img,
-                step = args.diffusion_steps,
+                (args.batch_size, 3, args.image_size, args.image_size), 
+                img,
+                step=args.diffusion_steps,
                 clip_denoised=args.clip_denoised,
                 model_kwargs=model_kwargs,
             )
 
             end.record()
             th.cuda.synchronize()
-            print('time for 1 sample', start.elapsed_time(end))  #time measurement for the generation of 1 sample
+            print(f'Time for sample {i+1}: {start.elapsed_time(end):.2f}ms')
 
             co = th.tensor(cal_out)
-            if args.version == 'new':
+            
+            # Use version parameter to determine output
+            if hasattr(args, 'version') and args.version == 'new':
                 enslist.append(sample[:,-1,:,:])
             else:
                 enslist.append(co)
 
             if args.debug:
-                # print('sample size is',sample.size())
-                # print('org size is',org.size())
-                # print('cal size is',cal.size())
                 if args.data_name == 'ISIC':
-                    # s = th.tensor(sample)[:,-1,:,:].unsqueeze(1).repeat(1, 3, 1, 1)
                     o = th.tensor(org)[:,:-1,:,:]
                     c = th.tensor(cal).repeat(1, 3, 1, 1)
-                    # co = co.repeat(1, 3, 1, 1)
-
                     s = sample[:,-1,:,:]
                     b,h,w = s.size()
                     ss = s.clone()
@@ -162,38 +389,41 @@ def main():
                     ss /= ss.max(1, keepdim=True)[0]
                     ss = ss.view(b, h, w)
                     ss = ss.unsqueeze(1).repeat(1, 3, 1, 1)
-
                     tup = (ss,o,c)
-                elif args.data_name == 'BRATS':
+                    
+                elif args.data_name in ['BRATS', 'BRATS3D']:
                     s = th.tensor(sample)[:,-1,:,:].unsqueeze(1)
-                    m = th.tensor(m.to(device = 'cuda:0'))[:,0,:,:].unsqueeze(1)
+                    m = th.tensor(m.to(device='cpu'))[:,0,:,:].unsqueeze(1)
                     o1 = th.tensor(org)[:,0,:,:].unsqueeze(1)
                     o2 = th.tensor(org)[:,1,:,:].unsqueeze(1)
                     o3 = th.tensor(org)[:,2,:,:].unsqueeze(1)
                     o4 = th.tensor(org)[:,3,:,:].unsqueeze(1)
                     c = th.tensor(cal)
-
                     tup = (o1/o1.max(),o2/o2.max(),o3/o3.max(),o4/o4.max(),m,s,c,co)
 
                 compose = th.cat(tup,0)
-                vutils.save_image(compose, fp = os.path.join(args.out_dir, str(slice_ID)+'_output'+str(i)+".jpg"), nrow = 1, padding = 10)
+                vutils.save_image(compose, fp=os.path.join(args.out_dir, f'{slice_ID}_output{i}.jpg'), nrow=1, padding=10)
+                
         ensres = staple(th.stack(enslist,dim=0)).squeeze(0)
-        vutils.save_image(ensres, fp = os.path.join(args.out_dir, str(slice_ID)+'_output_ens'+".jpg"), nrow = 1, padding = 10)
+        vutils.save_image(ensres, fp=os.path.join(args.out_dir, f'{slice_ID}_output_ens.jpg'), nrow=1, padding=10)
+        
+    logger.log("Sampling complete!")
 
 def create_argparser():
     defaults = dict(
-        data_name = 'BRATS',
+        data_name='BRATS3D',  # Changed default to match training
         data_dir="../dataset/brats2020/testing",
         clip_denoised=True,
         num_samples=1,
         batch_size=1,
         use_ddim=False,
-        model_path="",         #path to pretrain model
-        num_ensemble=5,      #number of samples in the ensemble
-        gpu_dev = "0",
+        model_path="",
+        num_ensemble=5,
+        gpu_dev="0",
         out_dir='./results/',
-        multi_gpu = None, #"0,1,2"
-        debug = False
+        multi_gpu=None,
+        debug=False,
+        version='new'  # Added version parameter
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()
@@ -202,5 +432,4 @@ def create_argparser():
 
 
 if __name__ == "__main__":
-
     main()
